a06fec3c7ffc24a626b2710cdc53985eb09e3d15
s230005 <gimpo.s230005@ggh.goe.go.kr>
2024-11-19 16:44:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,15 @@ import streamlit as st
 
 # 1. 기계학습 모델 파일 로드
 import joblib
-model = joblib.load
+model = joblib.load('linear_regression_model.pkl')
 # 2. 모델 설명
- 
+st.title('학업성취도 예측 지능 에이전트')
+st.subheader('모델 설명')
+st.write(' - 기계학습 알고리즘 : 선형 회귀 ')
+st.write(' - 학습 데이터 출처 : https://www.kaggle.com/')
+st.write(' - 훈련    데이터 : *건')
+st.write(' - 테스트 데이터 : *건')
+st.write(' - 인공지능 모델 정확도 : ***')
 
 # 3. 데이터시각화
 
