d01e247e6fba2bcb5aa4dda67c037fc204565095
s230005 <gimpo.s230005@ggh.goe.go.kr>
2024-11-22 14:05:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,16 @@ st.title('학업성취도 예측 지능 에이전트')
 st.subheader('모델 설명')
 st.write(' - 기계학습 알고리즘 : 선형 회귀 ')
 st.write(' - 학습 데이터 출처 : https://www.kaggle.com/')
-st.write(' - 훈련    데이터 : *건')
-st.write(' - 테스트 데이터 : *건')
-st.write(' - 인공지능 모델 정확도 : ***')
+st.write(' - 훈련    데이터 : 364건')
+st.write(' - 테스트 데이터 : 156건')
+st.write(' - 인공지능 모델 정확도 : 0.12')
 
 # 3. 데이터시각화
-
+with col1:
+      st.subheader('데이터시각화1')
+      st.image('시각화1.png' )
+with col2:
+      st.subheader('데이터시각화2')
+      st.image('시각화2.png' )
 
 # 4. 모델 활용
